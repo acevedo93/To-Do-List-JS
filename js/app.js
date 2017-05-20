15aca88a61aa2969afa61a3f2d4bc67e8a6452d4
david acevedo <dacevedojaramillo@gmail.com>
2017-05-20 16:26:47 -0300

    boton editar eliminado

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ var toDo =(function(){
  // entorno privado
         // arrays que contienen los objetos goals :array que se guarda en consola
                var goals = [];
-               //array que contiene lo que se muestra en pantalla 
+        //array que contiene lo que se muestra en pantalla
                var goalsScreen = [];
-              //constructores
+      //constructores
                function GoalsScreen() {
                 this.h3 = "";
                 this.id ="";
@@ -27,13 +27,13 @@ var toDo =(function(){
                 this.id = "";
                 this.complete = false;
                }
-        // funcion para añadir tareas // 
+        // funcion para añadir tareas //
                function addGoal(){
-             
+
                while(true){
                	 var goal = new Goal();
                	 goal.title = document.getElementById("entrada-titulo").value;
-                   if(goal.title === ""){
+                 if(goal.title === ""){
                     alert("no agregaste el titulo");
                     break;
                    }
@@ -43,112 +43,126 @@ var toDo =(function(){
                     break;
                    }
                  goal.id = document.getElementById("entrada-id").value;
-                   if(goal.id === ""){
-                    alert("no agregaste un id");
-                    break;
+                  if(goal.id === ""){
+                  alert("no agregaste un id");
+                  break;
                    }
                  goal.complete = false;
                  goals.push(goal);
                  break;
                 }
 
-               var container = document.getElementById("goals")
-               var goalScreen = new GoalsScreen();
+                var container = document.getElementById("goals")
+                var goalScreen = new GoalsScreen();
 
                 goalScreen.h3 = document.createElement("h3");
                 goalScreen.p = document.createElement("p");
                 goalScreen.li = document.createElement("li");
-
+                goalScreen.completar=document.createElement("button");
+                var completar = document.createTextNode("complete");
+                goalScreen.eliminar = document.createElement("button");
+                var eliminar = document.createTextNode("\u00D7");
                 goalScreen.id = document.createElement("h4");
-                goalScreen.button = document.createElement("button");
-                goalScreen.complete=document.createElement("button");
-                   
-                var xclose = document.createTextNode("\u00D7");
-                var complete= document.createTextNode("complete");
+
                for( var i = 0; i< goals.length ; i++){
-                 
+
         //escribiendo el contenido del array goals a goalsScreen
-         //para mostrarlo en pantalla 
+         //para mostrarlo en pantalla
 
                  goalScreen.h3.innerHTML = goals[i].title;
-                   goalScreen.li.appendChild(goalScreen.h3);
-
+                 goalScreen.li.appendChild(goalScreen.h3);
                  goalScreen.p.innerHTML = goals[i].description;
-                   goalScreen.li.appendChild(goalScreen.p);
-                goalScreen.id.innerHTML = goals[i].id;
-                   goalScreen.li.appendChild(goalScreen.id);
-                goalScreen.button.appendChild(xclose);
-                goalScreen.complete.appendChild(complete);
-                
-                goalScreen.li.appendChild(goalScreen.complete);
-                  goalScreen.li.appendChild(goalScreen.button);
+                 goalScreen.li.appendChild(goalScreen.p);
+                 goalScreen.id.innerHTML = goals[i].id;
+                 goalScreen.li.appendChild(goalScreen.id);
+                 goalScreen.completar.appendChild(completar);
+                 goalScreen.eliminar.appendChild(eliminar);
+                 goalScreen.li.appendChild(goalScreen.completar);
+                 goalScreen.li.appendChild(goalScreen.eliminar);
+                 goalScreen.li.className = "tarea";
+                 container.appendChild(goalScreen.li);
+                 goalsScreen.push(goalScreen);
+                 goalScreen.eliminar.addEventListener("click",eliminado);
+                 goalScreen.completar.addEventListener("click",completado);
+                 function  eliminado(){
+                   var div = this.parentElement;
+                   console.log(this);
+                    div.style.display = "none";
+                 };
+                 function completado(){
 
-                   goalScreen.li.className = "tarea";
-                container.appendChild(goalScreen.li);
-                goalsScreen.push(goalScreen);
+                   var div2 = this.parentElement;
+                   div2.style.background = "green";
+                 };
                 }
-               
-         
               }
 
-        //funcion para editar las tareas 
-              function editar(){
 
-          for(var i = 0; i < goalsScreen.length ; i++){
-        //la funcion xclose se encarga de eliminar las tareas de goalsScreen
-            goalsScreen[i].button.onclick = function xclose(){
-              var div = this.parentElement;
-                div.style.display = "none";
-              }
-        //la funcion completar  da por completada una tarea en goalsScreen
-            goalsScreen[i].complete.onclick = function completar(){
-                
-                var div2 = this.parentElement;
-                div2.style.background = "#B8DC3C";
-                 goals[i].complete = true;
-                
-              }
-          }
-        }
-                   
-       // funcion par amostrar todas las tareas  agregadas 
+       // funcion para mostrar todas las tareas  agregadas
           function showall(){
             var showall = document.getElementById("show-all")
-          for(var i = 0 ; i < goals.length ; i++){
-          var mostrarTodos = document.createElement("p");
-          mostrarTodos.className = "muestra";
-          mostrarTodos.innerHTML = goals[i].title;
-          showall.appendChild(mostrarTodos);
-          
-         }
+            for(var i = 0 ; i < goals.length ; i++){
+            var mostrarTodos = document.createElement("p");
+            mostrarTodos.className = "muestra";
+            mostrarTodos.innerHTML = goals[i].title;
+            showall.appendChild(mostrarTodos);
+
+           }
         }
 
 
  // entorno publico
  return{
  	iniciar:function(){
-  // boton de agregar 
+  // boton de agregar
        var btnAgregar = document.getElementById("agregar");
        btnAgregar.onclick = addGoal;
-  //boton de editar, que activa la funcion de completar y eliminar 
-       var edit = document.getElementById("editar");
-       edit.onclick = editar;
-   // boton de mostrar todos     
+   // boton de mostrar todos
        var mostrar = document.getElementById("mostrar-todos");
        mostrar.onclick = showall;
-      
+
+
  	  }
-    
+
   }
 
 
 })();
 
-// inicia el programa 
+// inicia el programa
 toDo.iniciar();
 
 
+/* jquery-----//
+ //cuando se carga el documento
+$(document).ready(function(){
+   console.log("cargando");
+});
+// seleecionando un elemento
 
+ var hola = $("p")
+ var holaVainilla = document.getElementsByTagName('p');
 
+ console.log(hola);
+ console.log(holaVainilla);
 
+ //seleccionando clases
 
+ var clases=$(".clases");
+ console.log(clases);
+
+//selector compuesto con clases y tags
+
+ var selector = $(".contents ul.people li");
+ console.log(selector);
+
+//seleccionando un elemento unico y mostrando el contenido html
+
+var selector2 = $('.hola li').eq(2).html();
+console.log(selector2);
+
+//seleccionando un elemento unico y mostrando el contenido texto plano
+
+var selector3 = $('.hola li').eq(0).text();
+console.log (selector3);
+*/
